27d8575f0971a4daf01b5aab433034fc1ba2e423
Evan Hadfield <evanhadfield@gmail.com>
2013-07-24 17:43:52 -0500

    got g.raphael working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require raphael
+//= require g.raphael
 //= require_tree .
 
 $('consumable-unit').click(function (event) {
---
+++ app/assets/javascripts/charts.js
@@ -0,0 +1,4 @@
+window.onload = function() {  
+    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
+    var circle = paper.circle(100, 100, 80);  
+}  
